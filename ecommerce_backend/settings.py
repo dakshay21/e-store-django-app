28c43ae3a66a994d7f7dc23366be501ba877124c
Dakshay <dakshay@auberginesolutions.com>
2026-03-24 21:53:49 +0530

    Attach static path

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -136,6 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# collectstatic output; must match docker-compose volume (static_volume -> /app/staticfiles)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
